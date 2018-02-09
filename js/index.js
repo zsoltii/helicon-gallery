34c737a3a37af8ea5087cdcbf447ab95e80fea4e
Fatér Zsolt <fater.zsolt@gmail.com>
2018-02-09 17:23:35 +0100

    helicon-gallery

---
+++ js/index.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    //$("#heliconGalley").backstretch("https://www.desktopbackground.org/download/o/2014/01/16/702386_green-alienware-backgrounds-wallpapers-para-hd-wallpapers-backgr_1920x1080_h.jpg")
+})
