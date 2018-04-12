eb8a714a918f114e44c15523c3fc67d1329a844f
Fatér Zsolt <fater.zsolt@gmail.com>
2018-04-12 14:41:14 +0200

    helicon-gallery

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@ var currentGallery = null;
 var galleryJson = null;
 var defaultLanguage = null;
 var currentLanguage = null;
+var currentPosition = null;
+var imagesLength = null;
 
 $(document).ready(function () {
-    currentGallery = getGallery();
+    currentGallery = getUrlParameter("gallery");
 
     $.getJSON("gallery/" + currentGallery + "/data.json")
         .done(function (data) {
@@ -12,51 +14,149 @@ $(document).ready(function () {
         }).fail(function (jqxhr, textStatus, error) {
         alert("Gallery data load error: " + textStatus);
     });
+
+    $('.goLeft a').click(function (e) {
+       currentPosition--;
+       loadImage();
+       e.preventDefault();
+    });
+
+    $('.goRight a').click(function (e) {
+        currentPosition++;
+        loadImage();
+        e.preventDefault();
+    });
+
+    $('.language button').click(function (e) {
+        if($('.language div.languageDropDown').hasClass('hidden')) {
+            $('.language div.languageDropDown').removeClass('hidden');
+        } else {
+            $('.language div.languageDropDown').addClass('hidden');
+        }
+        e.preventDefault();
+    });
+
+    $('.language a').click(function (e) {
+        $('.language div.languageDropDown').addClass('hidden');
+        changeLanguage($(this).attr('data-language'));
+        e.preventDefault();
+    })
 })
 
-function getGallery() {
+function getUrlParameter(parameter) {
     var url = new URL(location);
-    return '' + url.searchParams.get("gallery");
+    return '' + url.searchParams.get(parameter);
+}
+
+function getGalleryPosition() {
+    var url = new URL(location);
+    var hash = '' + location.hash;
+    if (hash.length > 0) {
+        hash = hash.substr(1);
+    }
+    return parseInt(hash);
 }
 
 function initGallery(data) {
     galleryJson = data;
 
-    defaultLanguage = galleryJson.defaultLanguage;
+    var urlLanguage = getUrlParameter("language").trim();
+    if (urlLanguage.length > 0 && galleryJson.languages.includes(urlLanguage)) {
+        defaultLanguage = urlLanguage;
+    } else {
+        defaultLanguage = galleryJson.defaultLanguage;
+    }
+
+    imagesLength = galleryJson.images.length;
+    initCurrentPosition();
     changeLanguage(defaultLanguage);
+}
+
+function initCurrentPosition() {
+    currentPosition = 0;
+    var urlPosition = getGalleryPosition();
+    if (!isNaN(urlPosition) && urlPosition > 1) {
+        if (urlPosition > imagesLength) {
+            currentPosition = imagesLength - 1;
+        } else {
+            currentPosition = urlPosition - 1;
+        }
+    }
 }
 
 function changeLanguage(language) {
     currentLanguage = language;
 
+    $('.logo div').removeClass();
+    $('.logo div').addClass(currentLanguage);
+
+    loadImage();
+    $('.language div.languageDropDown').addClass('hidden');
+    $('.language button').html($(".language a[data-language='" + currentLanguage + "']").html());
+    $('.language a').hide();
+    $.each(galleryJson.title, function (index, value) {
+        $(".language a[data-language='" + index + "']").show();
+    })
+}
+
+function loadImage() {
+    location.href = '#' + (currentPosition + 1);
+
     setTitle();
-    setImageTitle(0);
-    setDescription(0);
-    setStory(0);
-    setImage(0);
+    setImageTitle();
+    setDescription();
+    setStory();
+    setImage();
+    processNavigationVisibility();
 }
 
 function setTitle() {
-    newTitle = galleryJson.name[currentLanguage];
+    var hasTitle = galleryJson.title;
+    var newTitle = null;
+    if (hasTitle) {
+        newTitle = galleryJson.title[currentLanguage];
+        if ((newTitle == undefined) || newTitle == null) {
+            newTitle = galleryJson.title[defaultLanguage];
+        }
+    }
+
     if ((newTitle == undefined) || newTitle == null) {
-        newTitle = galleryJson.name[defaultLanguage];
+        $(".title").text('');
+    } else {
+        $(".title").text(newTitle);
+        document.title = newTitle;
     }
-    $(".title").text(newTitle);
 }
 
-function setImageTitle(position) {
-    newImageTitle = galleryJson.images[position].title[currentLanguage];
+function setImageTitle() {
+    var hasImageTitle = !(galleryJson.images[currentPosition].title == undefined);
+    var newImageTitle = null;
+
+    if (hasImageTitle) {
+        newImageTitle = galleryJson.images[currentPosition].title[currentLanguage];
+        if ((newImageTitle == undefined) || newImageTitle == null) {
+            newImageTitle = galleryJson.images[currentPosition].title[defaultLanguage];
+        }
+    }
+
     if ((newImageTitle == undefined) || newImageTitle == null) {
-        newImageTitle = galleryJson.images[position].title[defaultLanguage];
+        $(".imageTitle").text('');
+    } else {
+        $(".imageTitle").text(newImageTitle);
     }
-    $(".imageTitle").text(newImageTitle);
 }
 
-function setDescription(position) {
-    newDescription = galleryJson.images[position].story[currentLanguage];
-    if ((newDescription == undefined) || newDescription == null) {
-        newDescription = galleryJson.images[position].story[defaultLanguage];
+function setDescription() {
+    var hasDescription = !(galleryJson.images[currentPosition].description == undefined);
+    var newDescription = null;
+
+    if (hasDescription) {
+        newDescription = galleryJson.images[currentPosition].description[currentLanguage];
+        if ((newDescription == undefined) || newDescription == null) {
+            newDescription = galleryJson.images[currentPosition].description[defaultLanguage];
+        }
     }
+
     if ((newDescription == undefined) || newDescription == null) {
         $(".description").text('');
         $(".description").hide();
@@ -66,10 +166,15 @@ function setDescription(position) {
     }
 }
 
-function setStory(position) {
-    newStory = galleryJson.images[position].story[currentLanguage];
-    if ((newStory == undefined) || newStory == null) {
-        newStory = galleryJson.images[position].story[defaultLanguage];
+function setStory() {
+    var hasStory = !(galleryJson.images[currentPosition].story == undefined);
+    var newStory = null;
+
+    if (hasStory) {
+        newStory = galleryJson.images[currentPosition].story[currentLanguage];
+        if ((newStory == undefined) || newStory == null) {
+            newStory = galleryJson.images[currentPosition].story[defaultLanguage];
+        }
     }
     if ((newStory == undefined) || newStory == null) {
         $(".story").text('');
@@ -80,12 +185,25 @@ function setStory(position) {
     }
 }
 
-function setImage(position) {
-    newImageUrl = galleryJson.images[position].url;
+function setImage() {
+    newImageUrl = galleryJson.images[currentPosition].url;
     if ((newImageUrl == undefined) || newImageUrl == null) {
         alert("image data error");
     } else {
         $("#heliconGalley").backstretch(newImageUrl);
     }
+}
 
+function processNavigationVisibility() {
+    if(currentPosition == 0) {
+        $('.goLeft').hide();
+    } else {
+        $('.goLeft').show();
+    }
+
+    if (currentPosition == (imagesLength - 1)) {
+        $('.goRight').hide();
+    } else {
+        $('.goRight').show();
+    }
 }
